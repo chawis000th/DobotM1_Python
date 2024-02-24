f555b1f91894eacae34e726bfc95cc4ba3167b45
gunshipac130 <chawis_49@hotmail.com>
2024-02-24 15:45:13 +0700

    fix last_clean

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
+#### Changelog
+>- 20240218-1424: <br>
+>   - merged to main, split calculate function x,y <br>
+>- 20240220-1029: <br>
+>   - fix '_hsv' in 'def Mask_n_Draw' <br>
+>   - add 'contourArea' variable 'def Mask_n_Draw' <br>
+>- 20240220-1500: <br>
+>   - loop dobot <br>
+>- 20240221-1954: <br>
+>   - remove cv2 normal box function (unused) <br>
+>   - rearrange function order <br>
+>   - trim unuse variable <br>
+>   - rearrange object merge list <br>
+>- 20240222-0234: <br>
+>   - rearrange text <br>
+>- 20240224-1458: <br>
+>   - trim unuse library
+>- 20240224-1543: <br>
+>   - fix last_clean (really duplicate from frame)
+>   - fix double contour
+>   - move convert frame_hsv to inside mask_n_draw function
+
+>TO DO : <br>
+>   - add webcam manual focus
+***
+
 # DobotSDK_Python
 越疆机械臂的Python版本SDK，比官方API更强大，修复若干bug，支持多机协作
--- zzz_testing_ground.py
+++ zzz_testing_ground.py
@@ -1,4 +1,45 @@
-object_list = [[1],[2],[3],[],[7]]
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import time
 
-for i in object_list:
-    print(i[0])
+cap = cv2.VideoCapture(0)
+
+# .get image properties https://docs.opencv.org/3.4/d4/d15/group__videoio__flags__base.html#gaeb8dd9c89c10a5c63c139bf7c4f5704d
+vid_w = int(cap.get(3)) 
+vid_h = int(cap.get(4))
+vid_fps = int(cap.get(cv2.CAP_PROP_FPS)) # or use int(cap.get(5))
+vid_scale = 1 # image scale; 1=100% 0.5=50%
+print(f'video size = {vid_w} x {vid_h}\nframerate = {vid_fps}')
+
+cap.set(cv2.CAP_PROP_AUTO_EXPOSURE,0)
+cap.set(cv2.CAP_PROP_AUTOFOCUS,0)
+cap.set(cv2.CAP_PROP_AUTO_WB,0)
+
+
+
+
+while (cap.isOpened()):
+    # read and check for image availability before continue
+    check, frame = cap.read()
+    if check != True:
+        print('>> video ended or not available')
+        break
+    
+    cap.set(cv2.CAP_PROP_EXPOSURE,-100.0)
+    cap.set(cv2.CAP_PROP_FOCUS,5)
+    cap.set(cv2.CAP_PROP_BRIGHTNESS,255)
+    cap.set(cv2.CAP_PROP_CONTRAST,255)
+    cap.set(cv2.CAP_PROP_SATURATION,255)
+
+
+    # show window
+    cv2.imshow('RGB', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('e'):
+        print('>> Keyboard Exit')
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+print('>> Exit Phase 1')
